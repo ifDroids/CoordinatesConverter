69021eb762a67fb1f2d601d7d70999b0a3f01766
DamianosKalaitzidis <damianos.kalaitzidis@hotmail.com>
2020-03-14 20:19:26 +0200

    project: add views to binder

--- app/src/main/java/com/ibo/coordinatesconv/MainActivity.java
+++ app/src/main/java/com/ibo/coordinatesconv/MainActivity.java
@@ -1,22 +1,49 @@
 package com.ibo.coordinatesconv;
 
 import android.os.Bundle;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_debug)
+    Button mBtnDebug; //m for member of class
+    @BindView(R.id.input_lat)
+    EditText mInputlat;
+    @BindView(R.id.input_long)
+    EditText mInputlong;
+    @BindView(R.id.utm_x_output)
+    TextView mUtmxoutput;
+    @BindView(R.id.utm_y_output)
+    TextView mUtmyoutput;
+    @BindView(R.id.utm_zone_output)
+    TextView mUtmzone;
+
+//     EditText input_lat;
+//     EditText input_long;
+//     TextView utm_x_output;
+//     TextView utm_y_output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mBtnDebug.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String lat= mInputlat.getText().toString();
+                mUtmxoutput.setText(lat);
+                //mUtmxoutput.setText(mInputlat.getText().toString());
 
-
-
+            }
+        });
     }
-
 }
