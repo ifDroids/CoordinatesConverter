677438221a840b3744ce9e71ed26693a3434fe75
DamianosKalaitzidis <damianos.kalaitzidis@hotmail.com>
2020-03-30 20:41:34 +0300

    project: create listener for each button

--- app/src/main/java/com/ibo/coordinatesconv/MainActivity.java
+++ app/src/main/java/com/ibo/coordinatesconv/MainActivity.java
@@ -70,132 +70,112 @@ public class MainActivity extends AppCompatActivity {
 
         mBtninput_gnss.setBackground(getDrawable(R.drawable.round_frame_bar_option));
         mBtnoutput_utm.setBackground(getDrawable(R.drawable.round_frame_bar_option));
-    }
+    mBtnoutput_gnss.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mBtnoutput_gnss.setTextColor(getResources().getColor(R.color.green));
+            mBtnoutput_gnss.setBackground(getDrawable(R.drawable.round_frame_bar_option));
 
-    @OnClick(R.id.btn_debug)
-    public void onButtonClick(View v) {
-        if (x==1){
-            mUtmxoutput.setText(mInputlat.getText().toString());
-            Log.e("MainActivity", "DEBUG");}
-        else if(x==2){
-            Log.e("MainActivity", "sdasda");
+            mBtninput_gnss.setTextColor(getResources().getColor(R.color.red));
+            mBtnoutput_utm.setTextColor(getResources().getColor(R.color.red));
+            mBtnoutput_hepos.setTextColor(getResources().getColor(R.color.red));
+            mBtnoutput_utm.setBackground(null);
+            mBtnoutput_hepos.setBackground(null);
+            mUtm_zone_boundaries.setVisibility(View.GONE);
+            clearTextbutton();
         }
-        else if(x==3){
-            Log.e("MainActivity", "sdasda");
+    });
+    mBtnoutput_utm.setOnClickListener((new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mBtnoutput_gnss.setTextColor(getResources().getColor(R.color.red));
+            mBtnoutput_gnss.setBackground(null);
+
+            mBtninput_utm.setTextColor(getResources().getColor(R.color.red));
+
+            mBtnoutput_utm.setTextColor(getResources().getColor(R.color.green));
+            mBtnoutput_utm.setBackground(getDrawable(R.drawable.round_frame_bar_option));
+
+            mBtnoutput_hepos.setTextColor(getResources().getColor(R.color.red));
+            mBtnoutput_hepos.setBackground(null);
+            mUtm_zone_boundaries.setVisibility(View.VISIBLE);
+            clearTextbutton();
         }
-        else if(x==4){
-            Log.e("MainActivity", "sdasda");
+    }));
+    mBtnoutput_hepos.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mBtninput_hepos.setTextColor(getResources().getColor(R.color.red));
+
+            mBtnoutput_gnss.setTextColor(getResources().getColor(R.color.red));
+            mBtnoutput_gnss.setBackground(null);
+            mBtnoutput_utm.setTextColor(getResources().getColor(R.color.red));;
+            mBtnoutput_utm.setBackground(null);
+
+            mBtnoutput_hepos.setTextColor(getResources().getColor(R.color.green));
+            mBtnoutput_hepos.setBackground(getDrawable(R.drawable.round_frame_bar_option));
+
+            mUtm_zone_boundaries.setVisibility(View.GONE);
+            clearTextbutton();
         }
-        else  if(x==5){
-            Log.e("MainActivity", "sdasda");
+    });
+    mBtninput_gnss.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mBtninput_gnss.setTextColor(getResources().getColor(R.color.green));
+            mBtninput_gnss.setBackground(getDrawable(R.drawable.round_frame_bar_option));
+            mBtnoutput_gnss.setTextColor(getResources().getColor(R.color.red));
+            mBtninput_utm.setTextColor(getResources().getColor(R.color.red));
+            mBtninput_hepos.setTextColor(getResources().getColor(R.color.red));
+            mBtninput_utm.setBackground(null);
+            mBtninput_hepos.setBackground(null);
+            input_cor_text_lat.setText("Latitude");
+            input_cor_text_lon.setText("Longitude");
+            input_cor_text_alt.setText("Altitude");
+            clearTextbutton();
         }
-        else if(x==6){
-            Log.e("MainActivity", "sdasda");
+    });
+    mBtninput_utm.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mBtninput_utm.setTextColor(getResources().getColor(R.color.green));
+            mBtninput_utm.setBackground(getDrawable(R.drawable.round_frame_bar_option));
+
+            mBtnoutput_utm.setTextColor(getResources().getColor(R.color.red));
+
+            mBtninput_gnss.setTextColor(getResources().getColor(R.color.red));
+            mBtninput_hepos.setTextColor(getResources().getColor(R.color.red));
+            mBtninput_gnss.setBackground(null);
+            mBtninput_hepos.setBackground(null);
+            input_cor_text_lat.setText("X");
+            input_cor_text_lon.setText("Y");
+            input_cor_text_alt.setText("Z");
+            clearTextbutton();
         }
+    });
+    mBtninput_hepos.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mBtninput_gnss.setTextColor(getResources().getColor(R.color.red));
+            mBtninput_utm.setTextColor(getResources().getColor(R.color.red));
+            mBtninput_gnss.setBackground(null);
+            mBtninput_utm.setBackground(null);
+            mBtnoutput_hepos.setTextColor(getResources().getColor(R.color.red));
+            mBtninput_hepos.setTextColor(getResources().getColor(R.color.green));
+            mBtninput_hepos.setBackground(getDrawable(R.drawable.round_frame_bar_option));
+            input_cor_text_lat.setText("X");
+            input_cor_text_lon.setText("Y");
+            input_cor_text_alt.setText("Elevation");
+            clearTextbutton();
+        }
+    });
     }
 
-    @OnClick(R.id.btn_output_gnss)
-    public void onButtonoutput_gnss(View v) {
-        x=1;
-        mBtnoutput_gnss.setTextColor(getResources().getColor(R.color.green));
-        mBtnoutput_gnss.setBackground(getDrawable(R.drawable.round_frame_bar_option));
-
-        mBtninput_gnss.setTextColor(getResources().getColor(R.color.red));
-        mBtnoutput_utm.setTextColor(getResources().getColor(R.color.red));
-        mBtnoutput_hepos.setTextColor(getResources().getColor(R.color.red));
-        mBtnoutput_utm.setBackground(null);
-        mBtnoutput_hepos.setBackground(null);
-        mUtm_zone_boundaries.setVisibility(View.GONE);
-        clearTextbutton();
-    }
-
-    @OnClick(R.id.btn_output_utm)
-    public void onButtonbtn_output_utm(View v) {
-        x=2;
-        mBtnoutput_gnss.setTextColor(getResources().getColor(R.color.red));
-        mBtnoutput_gnss.setBackground(null);
-
-        mBtninput_utm.setTextColor(getResources().getColor(R.color.red));
-
-        mBtnoutput_utm.setTextColor(getResources().getColor(R.color.green));
-        mBtnoutput_utm.setBackground(getDrawable(R.drawable.round_frame_bar_option));
-
-        mBtnoutput_hepos.setTextColor(getResources().getColor(R.color.red));
-        mBtnoutput_hepos.setBackground(null);
-        mUtm_zone_boundaries.setVisibility(View.VISIBLE);
-        clearTextbutton();
-    }
-
-    @OnClick(R.id.btn_output_hepos)
-    public void onButtonoutput_hepos(View v) {
-        x=3;
-        mBtninput_hepos.setTextColor(getResources().getColor(R.color.red));
-
-        mBtnoutput_gnss.setTextColor(getResources().getColor(R.color.red));
-        mBtnoutput_gnss.setBackground(null);
-        mBtnoutput_utm.setTextColor(getResources().getColor(R.color.red));;
-        mBtnoutput_utm.setBackground(null);
-
-        mBtnoutput_hepos.setTextColor(getResources().getColor(R.color.green));
-        mBtnoutput_hepos.setBackground(getDrawable(R.drawable.round_frame_bar_option));
-
-        mUtm_zone_boundaries.setVisibility(View.GONE);
-        clearTextbutton();
-    }
-
-    @OnClick(R.id.btn_input_gnss)
-    public void onButtoninput_gnss(View v) {
-        x=4;
-        mBtninput_gnss.setTextColor(getResources().getColor(R.color.green));
-        mBtninput_gnss.setBackground(getDrawable(R.drawable.round_frame_bar_option));
-        mBtnoutput_gnss.setTextColor(getResources().getColor(R.color.red));
-        mBtninput_utm.setTextColor(getResources().getColor(R.color.red));
-        mBtninput_hepos.setTextColor(getResources().getColor(R.color.red));
-        mBtninput_utm.setBackground(null);
-        mBtninput_hepos.setBackground(null);
-        input_cor_text_lat.setText("Latitude");
-        input_cor_text_lon.setText("Longitude");
-        input_cor_text_alt.setText("Altitude");
-        clearTextbutton();
-    }
-
-    @OnClick(R.id.btn_input_utm)
-    public void onButtoninput_utm(View v) {
-        x=5;
-        mBtninput_utm.setTextColor(getResources().getColor(R.color.green));
-        mBtninput_utm.setBackground(getDrawable(R.drawable.round_frame_bar_option));
-
-        mBtnoutput_utm.setTextColor(getResources().getColor(R.color.red));
-
-        mBtninput_gnss.setTextColor(getResources().getColor(R.color.red));
-        mBtninput_hepos.setTextColor(getResources().getColor(R.color.red));
-        mBtninput_gnss.setBackground(null);
-        mBtninput_hepos.setBackground(null);
-        input_cor_text_lat.setText("X");
-        input_cor_text_lon.setText("Y");
-        input_cor_text_alt.setText("Z");
-        clearTextbutton();
-    }
-
-    @OnClick(R.id.btn_input_hepos)
-    public void onButtoninput_hepos(View v) {
-        x=6;
-        mBtninput_gnss.setTextColor(getResources().getColor(R.color.red));
-        mBtninput_utm.setTextColor(getResources().getColor(R.color.red));
-        mBtninput_gnss.setBackground(null);
-        mBtninput_utm.setBackground(null);
-        mBtnoutput_hepos.setTextColor(getResources().getColor(R.color.red));
-        mBtninput_hepos.setTextColor(getResources().getColor(R.color.green));
-        mBtninput_hepos.setBackground(getDrawable(R.drawable.round_frame_bar_option));
-        input_cor_text_lat.setText("X");
-        input_cor_text_lon.setText("Y");
-        input_cor_text_alt.setText("Elevation");
-        clearTextbutton();
     }
 
 
 
-}
+
 
 
 
